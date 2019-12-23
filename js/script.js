aaa530d31ba08a15cff50d9ccde25fa324703413
Oleg <espavoru@gmail.com>
2019-12-23 09:43:58 +0200

    Minor changes

--- js/script.js
+++ js/script.js
@@ -1,124 +1,50 @@
-jQuery(() => {
-  let flex = new Flex({
-    quont: 15,
-    text: [
-      "Mauris eget vulputate felis. Phasellus sit amet leo sed tellus vulputate",
-      "Voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos",
-      "Qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum",
-      "Auia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi.",
-      "Sed erat nibh, imperdiet in ornare et, feugiat nec diam. Morbi imperdiet.",
-      "Auia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi."
-    ]
+window.onload = () => {
+  let panel = new Panel({
+    content:
+      "Maecenas vehicula, nibh eget vehicula dictum, orci turpis tincidunt ligula",
+    id: "default"
   });
 
-  $("#app").append(flex.getElem());
+  document.querySelector("#app").appendChild(panel.getElem());
 
-  function Flex(options) {
-    let $elem,
-      $wrap,
-      $gridItems,
-      $item,
-      $imgWrap,
-      $img,
-      $text,
-      $after,
-      css,
-      $style,
-      target;
+  let panelW = new Panel({
+    content:
+      "Donec lobortis malesuada pellentesque. Etiam mattis ac libero a vestibulum",
+    id: "warning"
+  });
+  document.querySelector("#app").appendChild(panelW.getElem());
 
+  /**
+   * Create promo panel
+   */
+  function Panel(options) {
+    let elem, show;
+
+    /**
+     * Get elem
+     */
     function getElem() {
-      !$elem ? render() : null;
+      !elem ? render() : null;
 
-      return $elem;
+      return elem;
     }
 
+    /**
+     * Render element
+     */
     function render() {
-      $elem = $("<div>").addClass("flexGrid");
+      show = sessionStorage.getItem("show_" + options.id);
+      elem = document.createElement("div");
 
-      $wrap = $("<div>")
-        .addClass("flexGrid__wrap")
-        .css({
-          margin: "10px"
-        })
-        .appendTo($elem);
+      if (show !== null) return;
 
-      $gridItems = $("<div>")
-        .addClass("flexGrid__items")
-        .css({
-          display: "flex",
-          flexFlow: "row wrap",
-          margin: "0 -10px"
-        })
-        .appendTo($wrap);
+      elem.className = "promoPanel";
+      elem.textContent = options.content;
 
-      for (let i = 0; i < options.quont; i++) {
-        $item = $("<div>")
-          .addClass("flexGrid__item")
-          .css({
-            display: "flex",
-            flexFlow: "col wrap",
-            flex: "1 1 calc(50% - 30px)",
-            minWidth: 280,
-            margin: 10,
-            padding: 3,
-            backgroundColor: "#222",
-            transition: "all 500ms",
-            cursor: "pointer"
-          });
-
-        $imgWrap = $("<div>")
-          .addClass("flexGrid__imgWrap")
-          .css({
-            flex: "3 1 100%",
-            minWidth: 150
-          })
-          .appendTo($item);
-
-        $img = $("<img>")
-          .attr({
-            class: "flexGrid__img",
-            src: "https://picsum.photos/800/600?image=2" + i
-          })
-          .css({
-            display: "block"
-          })
-          .appendTo($imgWrap);
-
-        $text = $("<p>")
-          .addClass("flexGrid__text")
-          .css({
-            flex: "1 1 calc(30% - 10px)",
-            margin: "0 5px"
-          })
-          .text(options.text[i] || options.text[0] || "Lorem Ipsum")
-          .appendTo($item);
-
-        $gridItems.append($item);
-      }
-
-      $after = $item
-        .clone()
-        .css({
-          height: 0,
-          visibility: "hidden"
-        })
-        .appendTo($gridItems);
-
-      css = "";
-      css += "\n.flexGrid__item:hover {\n";
-      css += "  box-shadow: 2px 2px 7px rgba(0, 0, 0, 0.7)}\n";
-
-      $style = $("<style>")
-        .text(css)
-        .appendTo($("head"));
-
-      $elem.on("click", e => {
-        target = e.target.closest(".flexGrid__item");
-
-        target ? $(target).fadeOut(500) : null;
-      });
+      elem.addEventListener('click', () => {
+        this.hidden = true;
+        
+      })
     }
-
-    this.getElem = getElem;
   }
-});
+};
